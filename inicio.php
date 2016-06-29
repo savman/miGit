02cf292cf98cf67f4d3fe8e289bc9a2d99f2bba1
Santiago Vera <santiago@cangrejoverde.com>
2016-06-29 10:47:58 -0300

    modif para branch desarrollo1

---
+++ inicio.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Inicio</title>
+</head>
+<body>
+<h1>Inicio de prueba GIT</h1>
+<p>este es el primer archivo de prueba para GIT</p>
+<p>segunda  modificación para branch desarrollo1</p>
+</body>
+</html>
