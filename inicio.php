aed66d7c9c1a7d0672dfaa81a5f0dfbd17fac596
Santiago Vera <santiago@cangrejoverde.com>
2016-06-29 10:42:39 -0300

    primerphp

---
+++ inicio.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Inicio</title>
+</head>
+<body>
+<h1>Inicio de prueba GIT</h1>
+<p>este es el primer archivo de prueba para GIT</p>
+</body>
+</html>
